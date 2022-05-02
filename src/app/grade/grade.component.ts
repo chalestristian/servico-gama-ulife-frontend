8bf0fbfada8294cdef712dd1e3f0503709ee7ae2
Victor A. Lanni <victor.lanni@animaeducacao.com.br>
2022-05-01 21:34:37 -0300

    mudanças grade

--- src/app/grade/grade.component.ts
+++ src/app/grade/grade.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClientService } from '../services/http/http-client.service';
+
+import {Subject} from 'rxjs'; 
 
 @Component({
   selector: 'app-grade',
@@ -7,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradeComponent implements OnInit {
 
+  evaluations: any = [];
   selected: string = "Notas";
-  constructor() { }
+  dtTrigger: Subject<any> = new Subject<any>();
+  dtOptions: DataTables.Settings = {};
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private httpService: HttpClientService
+    ) { }
+
+  ngOnInit(): void {   
+    this.callApi();
+  }
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
+  callApi() {
+    this.httpService.get<any[]>("User/UserEvaluations/4")
+      .subscribe({
+        error: (e) => { console.log(e) },
+        next: (e) => { 
+          this.evaluations = e; 
+          this.dtTrigger.next(e);
+        }
+      }
+      ); 
+  } 
+      
+  dataAtualFormatada(value : string){
+    var data = new Date(value),
+        dia  = data.getDate().toString().padStart(2, '0'),
+        mes  = (data.getMonth()+1).toString().padStart(2, '0'), 
+        ano  = data.getFullYear(),
+        hora = data.getHours(),
+        minutos = data.getMinutes(),
+        segundos = data.getSeconds()
+
+    return dia +"/"+ mes +"/"+ ano +"-"+ hora +":"+ minutos +":"+ segundos;
 }
+} 
